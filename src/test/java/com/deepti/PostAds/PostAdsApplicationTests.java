d5907e4d7377e963ad5241e69129686bc6936d4c
Deepti Boddapati <deeptiboddapati@gmail.com>
2020-11-29 10:14:10 -0600

    Initial application
    Generated by https://start.spring.io/

---
+++ src/test/java/com/deepti/PostAds/PostAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepti.PostAds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
