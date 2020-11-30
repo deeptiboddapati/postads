c065749272b90d24389f5ec02e8d4c17db8265c5
Deepti Boddapati <deeptiboddapati@gmail.com>
2020-11-30 15:16:20 -0600

    Set up first API

--- src/main/java/com/deepti/PostAds/model/User.java
+++ src/main/java/com/deepti/PostAds/model/User.java
@@ -1,10 +1,13 @@
 package com.deepti.PostAds.model;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 public class User {
     private final UUID id;
     private final String username;
 
-    public User(UUID id, String username) {
+    public User(@JsonProperty("id") UUID id,
+                @JsonProperty("username") String username) {
         this.id = id;
         this.username = username;
     }
